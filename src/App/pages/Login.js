e0dcc6726fd001256abf1af7cf897a24339fbdcb
Andrii <andrii@display.io>
2018-08-21 17:02:46 +0300

    Fix Login layout

--- src/App/pages/Login.js
+++ src/App/pages/Login.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import backgroundImg from '../../../static/assets/login-page/team/@1x.png';
+
 const Login = ({loginForm}) => {
   return (
     <div className="l-page l-page-bg-white">
@@ -8,6 +10,7 @@ const Login = ({loginForm}) => {
         <div className="wave"/>
         <div className="l-login">
           {loginForm}
+          <img className="l-login__background" src={backgroundImg} alt=""/>
         </div>
       </div>
     </div>
