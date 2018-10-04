122077259b0e5c3caf040323616cce0c1ea338f1
Andrii <andrii@display.io>
2018-10-04 13:22:55 +0300

    Refactor AppEdit reducer to use redux-actions

--- src/App/redux/ui/Applications/editing.js
+++ src/App/redux/ui/Applications/editing.js
@@ -1,58 +1,41 @@
-import { APP_EDIT, APP_MODAL_SHOW, APP_MODAL_HIDE, APP_MODAL_LOADER } from '../../action-types';
+import { createAction, handleActions } from 'redux-actions';
 
-const initialAppState = {
+export const appEdit = createAction('APP_EDIT', id => id);
+
+export const appModalShow = createAction('APP_MODAL_SHOW');
+
+export const appModalHide = createAction('APP_MODAL_HIDE');
+
+export const loader = createAction('APP_MODAL_LOADER', isLoader => isLoader);
+
+const initialState = {
   modal: false,
 };
 
-const appEditReducer = (state = initialAppState, action) => {
-  switch (action.type) {
-    case APP_EDIT:
-      return {
-        ...state,
-        modal: true,
-        id: action.payload,
-      };
-    case APP_MODAL_SHOW:
-      return {
-        ...state,
-        modal: true,
-      };
-    case APP_MODAL_HIDE:
-      return {
-        ...state,
-        modal: false,
-        id: null,
-      };
-    case APP_MODAL_LOADER:
-      return {
-        ...state,
-        loader: action.payload,
-      };
-    default:
-      return state;
-  }
-};
-
-export const appModalShow = () => ({
-  type: APP_MODAL_SHOW,
-});
-
-export const appModalHide = () => ({
-  type: APP_MODAL_HIDE,
-});
-
-export const loader = (isLoader) => ({
-  type: APP_MODAL_LOADER,
-  payload: isLoader
-});
-
-export const appEdit = (id) => ({
-  type: APP_EDIT,
-  payload: id
-});
+export default handleActions(
+  {
+    [appEdit]: (state, action) => ({
+      ...state,
+      modal: true,
+      id: action.payload,
+    }),
+    [appModalShow]: state => ({
+      ...state,
+      modal: true,
+    }),
+    [appModalHide]: state => ({
+      ...state,
+      modal: false,
+      id: null,
+    }),
+    [loader]: (state, action) => ({
+      ...state,
+      loader: action.payload,
+    }),
+  },
+  initialState,
+);
 
 export const getIdAppEdit = state => state.ui.applications.editing.id;
 export const getIsAppModalShow = state => state.ui.applications.editing.modal;
 export const getLoaderState = state => state.ui.applications.editing.loader;
-
-export default appEditReducer;
