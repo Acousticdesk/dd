447b5c2f170589320916012f51caf39def37ae4b
Andrii <andrii@display.io>
2018-10-04 12:51:08 +0300

    Refactor reducer names

--- src/App/redux/reducer.js
+++ src/App/redux/reducer.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
-import applicationsDataReducer from './data/Applications/reducer';
+import applicationsDataReducer from './data/Applications';
 import entitiesReducer from './data/entities';
 import mobileSidebarReducer from './ui/mobileSidebar';
 import mobileViewportReducer from './ui/mobileViewport';
-import applicationsUIReducer from './ui/Applications/reducer';
+import applicationsUIReducer from './ui/Applications';
 
 const uiReducer = combineReducers({
   applications: applicationsUIReducer,
