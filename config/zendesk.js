e30aaf06800752785a6480cdbf9f5e176aac900f
Andrii <andrii@display.io>
2018-10-10 18:38:30 +0300

    Decompose main config

---
+++ config/zendesk.js
@@ -0,0 +1,24 @@
+const ZENDESK_URL = 'https://support.display.io/hc/';
+const ARTICLES_PATH = 'en-us/articles/';
+const API_URL = `${ZENDESK_URL}${ARTICLES_PATH}360001659993`;
+
+export default {
+  zendesk: {
+    general: ZENDESK_URL,
+    ios: {
+      Interstitial: `${ZENDESK_URL}${ARTICLES_PATH}360001560513-Interstitial`,
+    },
+    android: {
+      InFeed: `${ZENDESK_URL}${ARTICLES_PATH}360001478974-In-Feed`,
+      Interstitial: `${ZENDESK_URL}${ARTICLES_PATH}360001478934-Interstitial`,
+      RewardedVideo: `${ZENDESK_URL}${ARTICLES_PATH}360001544534-Rewarded-Video`,
+      Native: `${ZENDESK_URL}${ARTICLES_PATH}360001465653-Native-Ads`,
+    },
+    api: {
+      InFeed: API_URL,
+      Interstitial: API_URL,
+      RewardedVideo: API_URL,
+      Native: API_URL,
+    },
+  },
+};
